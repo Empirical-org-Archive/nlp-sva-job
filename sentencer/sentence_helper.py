3182e1b776dba8616ad7bfc4c847ef352b4c1327
Eric Tang <erictang1729@gmail.com>
2018-07-27 16:08:59 -0400

    Add sentencer, update publisher.py file

---
+++ sentencer/sentence_helper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests, zipfile, io
+import spacy
+
+def get_sentences(data):
+    link = data['link']
+    r = requests.get(link)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    for fname in z.namelist():
+        text = str(z.open(fname).read())
+        return get_sents_from_text(text)
+
+def get_sents_from_text(text):
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(text)
+    return list(doc.sents)
