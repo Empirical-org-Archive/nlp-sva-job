38c0851a8836803c0ae3e03b52d099b7d353f09f
Max Buck <maxwellhigginsbuck@gmail.com>
2018-08-06 18:18:47 -0400

    spacy for reducer and sentencer; move spacy load line in helper; add http protocol to each link

--- sentencer/sentence_helper.py
+++ sentencer/sentence_helper.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 import requests, zipfile, io
 import spacy
+nlp = spacy.load('en_core_web_sm')
 
 def get_sentences(link):
-    r = requests.get(link)
+    r = requests.get('http://{}'.format(link))
     z = zipfile.ZipFile(io.BytesIO(r.content))
     for fname in z.namelist():
         text = str(z.open(fname).read())
         return get_sents_from_text(text)
 
 def get_sents_from_text(text):
-    nlp = spacy.load('en_core_web_sm')
     doc = nlp(text)
     return list(doc.sents)
