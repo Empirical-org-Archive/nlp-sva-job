139712844241e562ad1c0084d57a32f179b434e8
Eric Tang <erictang1729@gmail.com>
2018-08-06 17:33:42 -0400

    Temporarily comment out reducers in start.sh script for testing

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # ensure envars are loaded
 source /root/.bash_profile
 
-# set droplet ID 
+# set droplet ID
 export DROPLET_UID=$(curl -s http://169.254.169.254/metadata/v1/id)
 export DROPLET_NAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
 
@@ -13,9 +13,9 @@ autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -L 5432:l
 #autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -L 5000:localhost:5000 root@206.81.5.140 &
 
 # wait for ssh tunnels to be created, ready to go
-sleep 10s 
+sleep 10s
 
-# test connection to api, attempt to establish one if it doesn't exist 
+# test connection to api, attempt to establish one if it doesn't exist
 #curl localhost:5000 || autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -N -L 5000:localhost:5000 root@206.81.5.140 &
 
 # start the system monitor script
@@ -38,35 +38,35 @@ do
   nohup /var/lib/jobs/$JOB_NAME/sentencer/venv/bin/python3 /var/lib/jobs/$JOB_NAME/sentencer/sentencer.py &
   extractor_processes+=($!)
 done
-
-# give a second for some sentences to pile up, but don't wait for sentencing to
-# complete before starting to reduce
-sleep 1m 
-
-# start pre-reduction publisher
-nohup /var/lib/jobs/$JOB_NAME/reducer/venv/bin/python3 /var/lib/jobs/$JOB_NAME/reducer/publisher.py &
-prereduction_publisher_process=$!
-
-# start reduction writer
-nohup /var/lib/jobs/$JOB_NAME/reducer/venv/bin/python3 /var/lib/jobs/$JOB_NAME/reducer/writer.py &
-reduction_writer_process=$!
-
-# Start x reducers
-cpu_count=$(grep -c ^processor /proc/cpuinfo)
-worker_count=$(( cpu_count / 1 ))
-reducer_processes=()
-for i in $(seq 1 $worker_count)
-do
-  nohup /var/lib/jobs/$JOB_NAME/reducer/venv/bin/python3 /var/lib/jobs/$JOB_NAME/reducer/reducer.py &
-  reducer_processes+=($!)
-done
-
-# wait until reduction is complete
-while [ true ]
-do
-    sleep 1m
-    export r=$(curl --user $JM_USER:$JM_PASS $JOB_MANAGER/jobs/$JOB_ID/state) && [ $r == \"reduced\" ] && break || continue
-done
+#
+# # give a second for some sentences to pile up, but don't wait for sentencing to
+# # complete before starting to reduce
+# sleep 1m
+#
+# # start pre-reduction publisher
+# nohup /var/lib/jobs/$JOB_NAME/reducer/venv/bin/python3 /var/lib/jobs/$JOB_NAME/reducer/publisher.py &
+# prereduction_publisher_process=$!
+#
+# # start reduction writer
+# nohup /var/lib/jobs/$JOB_NAME/reducer/venv/bin/python3 /var/lib/jobs/$JOB_NAME/reducer/writer.py &
+# reduction_writer_process=$!
+#
+# # Start x reducers
+# cpu_count=$(grep -c ^processor /proc/cpuinfo)
+# worker_count=$(( cpu_count / 1 ))
+# reducer_processes=()
+# for i in $(seq 1 $worker_count)
+# do
+#   nohup /var/lib/jobs/$JOB_NAME/reducer/venv/bin/python3 /var/lib/jobs/$JOB_NAME/reducer/reducer.py &
+#   reducer_processes+=($!)
+# done
+#
+# # wait until reduction is complete
+# while [ true ]
+# do
+#     sleep 1m
+#     export r=$(curl --user $JM_USER:$JM_PASS $JOB_MANAGER/jobs/$JOB_ID/state) && [ $r == \"reduced\" ] && break || continue
+# done
 
 
 # TODO: bad code, remove this - fix should be in reducers where job state is
@@ -77,7 +77,3 @@ sleep 10m
 # the droplet is no longer needed, droplet makes
 # a DELETE request on itself.
 curl --user $JM_USER:$JM_PASS -X DELETE $JOB_MANAGER/droplets/$DROPLET_UID
-
-
-
-
