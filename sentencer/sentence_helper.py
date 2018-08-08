4e1c2e1b7c48ec4f57d28614bffdad99e73d97f8
Max Buck <maxwellhigginsbuck@gmail.com>
2018-08-08 16:38:08 -0400

    replace new lines and escaped r characters with whitespace

--- sentencer/sentence_helper.py
+++ sentencer/sentence_helper.py
@@ -13,6 +13,7 @@ def get_sentences(link):
     z = zipfile.ZipFile(io.BytesIO(r.content))
     for fname in z.namelist():
         text = str(z.open(fname).read())
+        text = text.replace('\r\n', ' ').replace('\r',' ').replace('\n', ' ')
         return get_sents_from_text(text)
 
 def get_sents_from_text(text):
