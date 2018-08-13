ed3ff8421a33e0a541c9c506881e72fd2b4950d9
Eric Tang <erictang1729@gmail.com>
2018-08-13 12:09:57 -0400

    Get reductions from test sentence

--- reducer/classifier-api.py
+++ reducer/classifier-api.py
@@ -1,5 +1,4 @@
 #TODO: Move this file to another folder
-
 from flask import Flask
 from reducer_helper import get_reduction, load_predictor
 
@@ -8,4 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     test_sent = "The Expo marker was a bland purple that triggered her intense synesthesia."
-    return test_sent
+    print("Test sent:", test_sent)
+    predictor = load_predictor(path="elmo-constituency-parser-2018.03.14.tar.gz")
+    return str(get_reduction(test_sent, predictor))
