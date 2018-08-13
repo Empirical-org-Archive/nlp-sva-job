447dce9d83d1bcfdeaef4a02bd99a9615154debd
Eric Tang <erictang1729@gmail.com>
2018-08-13 12:46:35 -0400

    Return counts in API

--- reducer/classifier-api.py
+++ reducer/classifier-api.py
@@ -1,12 +1,35 @@
 #TODO: Move this file to another folder
 from flask import Flask
+from flask import request
 from reducer_helper import get_reduction, load_predictor
+import psycopg2
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+# Connect to the database
+try:
+    DB_NAME = os.environ.get('SVA_DB_NAME', 'sva')
+    DB_PASSWORD = os.environ.get('SVA_DB_PASS', '')
+    DB_USER = os.environ.get('SVA_DB_USER', 'etang')
+except KeyError as e:
+    print('important environment variables were not set')
+    raise Exception('Warning: Important environment variables were not set')
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host='localhost')
+cur = conn.cursor()
+
+predictor = load_predictor(path="elmo-constituency-parser-2018.03.14.tar.gz")
+
+@app.route("/", methods=["GET"])
 def hello():
-    test_sent = "The Expo marker was a bland purple that triggered her intense synesthesia."
-    print("Test sent:", test_sent)
-    predictor = load_predictor(path="elmo-constituency-parser-2018.03.14.tar.gz")
-    return str(get_reduction(test_sent, predictor))
+    sentence = request.args['sentence']
+    print("Sentence: ", sentence)
+    reductions = get_reduction(sentence, predictor)
+    return str([get_count(r) for r in reductions])
+
+def get_count(reduction):
+    cur.execute("""SELECT count FROM reductions_to_count_tmp WHERE
+                    reduction=%s""", (reduction, ))
+    row = cur.fetchone()
+    return row[0] if row else 0
