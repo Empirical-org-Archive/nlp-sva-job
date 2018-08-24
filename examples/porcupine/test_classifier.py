a6bbfb472a57f6b292c6fd4d5658f7ff0fcc29d5
Eric Tang <erictang1729@gmail.com>
2018-08-24 11:34:17 -0400

    Add test for language tool spelling feedback

--- examples/porcupine/test_classifier.py
+++ examples/porcupine/test_classifier.py
@@ -4,11 +4,15 @@ from check import check, Feedback
 
 class TestClassifierMethods(unittest.TestCase):
 
-    def test_check_sva(self):
+    def test_check_sva_feedback(self):
         sent = "The girl run incredibly fast."
         actual_fb = check(sent)
-        expected_fb = Feedback()
         self.assertEqual(actual_fb.primary_error, 'SUBJECT_VERB_AGREEMENT_ERROR')
+
+    def test_check_spelling_feedback(self):
+        sent = "The scientest made a spelling mistake."
+        actual_fb = check(sent)
+        self.assertEqual(actual_fb.primary_error, 'SPELLING_ERROR')
 
 if __name__ == '__main__':
     unittest.main()
